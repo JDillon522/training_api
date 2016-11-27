290f3034d64dd8ffd01e5083f2eda8080e06dfe6
Joseph Dillon <joseph.dillon.522@gmail.com>
2016-11-27 22:13:29 +0200

    implemented basic firebase stuff

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var firebase = require('firebase-admin');
+var admin = require('firebase-admin');
+var key = require('../bin/firebaseKey.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  admin.initializeApp({
+    credential: admin.credential.cert(key),
+    databaseURL: "https://training-api-76609.firebaseio.com/"
+  });
+
+  var db = admin.database();
+  var ref = db.ref("/api");
+
+  var namesRef = ref.child('/names')
+
+  namesRef.once("value", function(snapshot) {
+    console.log(snapshot.val());
+
+    res.render('index', { title: 'Express', names: snapshot.val() });
+  });
+
 });
 
 module.exports = router;
