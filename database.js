d0cd3c9d12b6b81423258981cb28548dbdfcae5e
Joseph Dillon <joseph.dillon.522@gmail.com>
2016-12-31 20:11:50 +0200

    figured out abstraction problem

---
+++ database.js
@@ -0,0 +1,21 @@
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user: process.env.DB_USER, //env var: PGUSER
+    database: process.env.DB_NAME, //env var: PGDATABASE
+    password: process.env.DB_PASSWORD, //env var: PGPASSWORD
+    host: process.env.DB_HOST, // Server hosting the postgres database
+    port: process.env.DB_PORT, //env var: PGPORT
+    // max: 10, // max number of clients in the pool
+    // idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    ssl: true
+  }
+});
+var bookshelf = require('bookshelf')(knex);
+
+var User = bookshelf.Model.extend({
+  tableName: 'users',
+  // hasTimestamps: true,
+});
+
+module.exports.User = User;
