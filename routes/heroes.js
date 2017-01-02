db881a37c15c2240ad9fcd12b58a106eeabc67fd
Joseph Dillon <joseph.dillon.522@gmail.com>
2017-01-02 12:51:49 +0200

    added heroes CRUD

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 var db = require('../database');
 
-/* GET users listing. */
+
+/* GET heros listing. */
 router.get('/', function(req, res, next) {
   fetchAllHeroes(function(result, status) {
-    return res.send(result);
+    return res.status(status).send(result).end();
   });
 });
 
@@ -15,26 +15,77 @@ router.get('/:hero_id', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+  var hero = {
+    name: req.body.name,
+    age: parseInt(req.body.age, 10),
+    power_id: parseInt(req.body.power_id, 10)
+  };
+  addHero(hero, function(result, status) {
+    return res.send(result);
+  });
+});
+
+router.delete('/:hero_id', function(req, res, next) {
+  deleteHero(req.params.hero_id, function(result, status) {
+    return res.send(result).status(200).end();
+  });
+});
 
 var fetchAllHeroes = function(callback) {
-  db.Heroes.fetchAll().then( function (success) {
-    return callback(success, 200);
-  }, function (error) {
-    return callback(new Error(error), 400);
+  db.query('SELECT * FROM heroes', function(error, result) {
+    if (error) {
+      console.error(error);
+      return callback(new Error(error), 400);
+    }
+
+    return callback(result.rows, 200);
   });
 };
 
 var fetchHero = function(id, callback) {
-  db.Heroes.query({where: {'id' : id}}).fetch().then( function (success) {
-    return callback(success, 200);
-  }, function (error) {
-    return callback(new Error(error), 400);
+  db.query('SELECT * FROM heroes WHERE id = ' + id, function(error, result) {
+    if (error) {
+      console.error(error);
+      return callback(new Error(error), 400);
+    }
+
+    return callback(result.rows, 200);
+  });
+};
+
+var addHero = function(hero, callback) {
+  var query = 'INSERT INTO heroes (name, age, power_id) VALUES (\'' +
+    hero.name + '\', \'' +
+    hero.age + '\', \'' +
+    hero.power_id + '\')';
+
+  db.query(query, function(error, result) {
+    if (error) {
+      console.error(error);
+      return callback(new Error(error), 400);
+    }
+
+    return callback(result, 200);
   });
 }
 
+var deleteHero = function(id, callback) {
+  var query = 'DELETE FROM heroes WHERE id = ' + id;
+
+  db.query(query, function(error, result) {
+    if (error) {
+      console.error(error);
+      return callback(new Error(error), 400);
+    }
+
+    return callback(result, 200);
+  });
+};
+
 // Lib methods
-module.exports.heroes = {};
-module.exports.heroes.fetchAllHeroes = fetchAllHeroes;
-module.exports.heroes.fetchHero = fetchHero;
+module.exports.heros = {};
+module.exports.heros.fetchAllHeroes = fetchAllHeroes;
+module.exports.heros.fetchHero = fetchHero;
 
 module.exports = router;
